2bf2f6b95d875f526a791214b5631860efdc01b8
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-10-19 20:09:33 +0530

    TestNg 1

--- src/test/java/tests/CheckOutFlow.java
+++ src/test/java/tests/CheckOutFlow.java
@@ -18,37 +18,46 @@ public class CheckOutFlow extends BaseTest {
         WebDriver driver = check.getDriver();
 
         try{
+            // Open website
             driver.get("https://www.saucedemo.com");
+
+            // Login
             driver.findElement(By.id("user-name")).sendKeys("standard_user");
             driver.findElement(By.id("password")).sendKeys("secret_sauce");
-
             driver.findElement(By.id("login-button")).click();
 
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //wait for products to appear on page after login
+
+            //Add items to cart
             driver.findElement(By.cssSelector("[data-test='add-to-cart-sauce-labs-backpack']")).click();
             driver.findElement(By.xpath("//button[contains(@id,'bike-light')]")).click();
 
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            //wait until cart badge visible, then go to cart
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("shopping_cart_badge")));
-
             driver.findElement(By.className("shopping_cart_link")).click();
 
             // wait until checkout button is clickable
             WebElement checkoutbtn = wait.until(ExpectedConditions.elementToBeClickable(By.id("checkout")));
             checkoutbtn.click();
 
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[2]/span")));
+            //Fill in details after waiting for first element to appear
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("first-name")));
 
             driver.findElement(By.id("first-name")).sendKeys("Test");
             driver.findElement(By.id("last-name")).sendKeys("TestLast");
             driver.findElement(By.id("postal-code")).sendKeys("401305");
             driver.findElement(By.id("continue")).click();
 
+
+            //finish order
             WebElement finishbtn = wait.until(ExpectedConditions.elementToBeClickable(By.id("finish")));
             finishbtn.click();
 
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("complete-header")));
+            boolean orderComplete = driver.findElement(By.id("back-to-products")).isDisplayed();
 
-            if(driver.findElement(By.id("back-to-products")).isDisplayed()){
+            if(orderComplete){
                 System.out.println("Test Passed, Congratulations");
             }
 
